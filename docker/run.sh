ff2979cdc56dd2325be94739d65d8ec16f1c0b89
Marco Pivetta <ocramius@gmail.com>
2014-04-07 00:13:42 +0200

    Adding comments to make the runner more explicit

--- docker/run.sh
+++ docker/run.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
+# cleaning up state
 docker kill NGINX
 docker kill HHVM
-
 docker rm HHVM
 docker rm NGINX
-
 docker rmi hhvm
 docker rmi nginx
 
+# build containers (install dependencies, setup configurations, etc)
 docker build -t hhvm HHVM
 docker build -t nginx NGINX
 
+# run the containers
 docker run -d --name HHVM hhvm
 docker run -d -p 11000:80 --name NGINX --link HHVM:PHP nginx
