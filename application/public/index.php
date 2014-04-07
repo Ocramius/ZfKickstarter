11d12793748d6a45b5b93be056232f80e0759030
Marco Pivetta <ocramius@gmail.com>
2014-04-07 05:00:58 +0200

    Fixed autoloader inclusion (file path was wrong)

--- application/public/index.php
+++ application/public/index.php
@@ -4,7 +4,6 @@ use Zend\Mvc\Application;
 
 chdir(dirname(__DIR__));
 
-require __DIR__ . '/../vendor/autoloader.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-// Run the application!
 Application::init(require __DIR__ . '/../config/application.config.php')->run();
